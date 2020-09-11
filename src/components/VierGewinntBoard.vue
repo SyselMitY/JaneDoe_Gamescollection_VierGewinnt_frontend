7e7de0a6469b8c5e82ee8c0e0090131dce64a04f
Peter Sysel <peter@sysel.at>
2020-09-11 12:01:06 +0200

    board begin

---
+++ src/components/VierGewinntBoard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="vierGewinntBoard">
+        <div class="boardRow" v-for="(boardArrayRow,rowIndex) in boardArray" :key="rowIndex">
+            <div class="boardCell" v-for="(boardCell,columnIndex) in boardArrayRow"
+                 :key="rowIndex*10 + columnIndex"
+                ></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "VierGewinntBoard",
+        props: ["boardArray"],
+    }
+</script>
+
+<style scoped>
+    .boardCell {
+        width: 2rem;
+        height: 2rem;
+        border: solid black 2px;
+        border-radius: 5px;
+    }
+
+    .boardRow {
+        display: flex;
+    }
+</style>
---
+++ src/views/ViewBoard.vue
@@ -0,0 +1,20 @@
+<template>
+    <VierGewinntBoard :board-array="boardArray"></VierGewinntBoard>
+</template>
+
+<script>
+    import VierGewinntBoard from "@/components/VierGewinntBoard";
+    export default {
+        name: "ViewBoard",
+        components: {VierGewinntBoard},
+        data() {
+            return {
+                boardArray: [[1,2],[1,0]]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
